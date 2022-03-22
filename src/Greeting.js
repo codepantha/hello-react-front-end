506a213496cb0d065e5a7e39fc49769cf1ddd688
codepanther <pezechigozirim@gmail.com>
2022-03-22 23:52:26 +0100

    consume the rails api to display random greetings

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import { getAllGreetings, getRandomGreeting } from './redux/greetings';
 
 const Greeting = () => {
   const location = useLocation();
+  const dispatch = useDispatch();
+  const { loading, greetings } = useSelector((state) => state.greetingsReducer);
 
-  if (location.pathname === '/') return (<div>All Greetings</div>);
+  useEffect(() => {
+    if (location.pathname === '/') dispatch(getAllGreetings());
+    else dispatch(getRandomGreeting());
+  }, [location.pathname, dispatch]);
 
   return (
-    <div>Greeting</div>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Greetings</h1>
+      {!loading && (
+        greetings.map((greeting) => <p key={uuidv4()}>{greeting.greeting}</p>)
+      )}
+    </div>
   );
 };
 
